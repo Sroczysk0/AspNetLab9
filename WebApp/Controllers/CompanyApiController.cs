4bfdea74314ff8738b3b21320b616f7e2a38b751
MagdaSroka <138120680+Sroczysk0@users.noreply.github.com>
2024-11-26 12:37:55 +0100

    ..

---
+++ WebApp/Controllers/CompanyApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Model.Movies;
+
+namespace WebApp.Controllers;
+[ApiController]
+[Route("/api/companies")]
+public class CompanyApiController : ControllerBase
+{
+    private MoviesDbContext _context;
+}
